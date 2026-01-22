bf5e9ae205882dd58379db1c63d98e1e45992aff
parmarsiddharth816-png <parmarsiddharth816@gmail.com>
2026-01-22 12:46:04 +0530

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,8 +1,15 @@
 function startVoice() {
-  let rec = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-  rec.lang = "en-US";
-  rec.start();
-  rec.onresult = e => {
-    userInput.value = e.results[0][0].transcript;
+  if (!("webkitSpeechRecognition" in window)) {
+    alert("Voice not supported in this browser");
+    return;
+  }
+
+  const recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-US";
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    document.getElementById("chat-input").value =
+      event.results[0][0].transcript;
   };
 }
